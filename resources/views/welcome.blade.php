1d2e822b92c8f089d5616e6430fdba5532f5a891
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-11-05 11:35:53 +0200

    Add post cards and pagination

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,25 @@
 @section('title', 'Home page')
 @section('content')
     <div class="container mx-auto">
-        <ul>
-            @foreach($posts as $post)
-                <li class="list-disc">{{$post->title}}</li>
+        <div class="my-2 text-center">
+        {{ $posts->links() }}
+        </div>
+        <div class="grid grid-cols-4 gap-4">
+            @foreach ($posts as $post)
+                <div class="card bg-base-300 shadow-xl">
+                    <figure>
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">{{ $post->title }}</h2>
+                        <p>{{ $post->snippet }}</p>
+                        <p class="text-neutral-content">{{$post->created_at->diffForHumans()}}</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-
-        </ul>
+        </div>
     </div>
 @endsection
